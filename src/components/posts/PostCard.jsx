926d93ada22563e67f53737f99886e35e9c6424a
Rakibul12356 <mdrakibulhasan12346@gmail.com>
2025-06-21 01:10:41 +0600

    create basic post card

--- src/components/posts/PostCard.jsx
+++ src/components/posts/PostCard.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import PostHeader from './PostHeader';
+import PostBody from './PostBody';
+import PostAction from './PostAction';
+import PostComments from './PostComments';
+import PostCommentList from './PostCommentList';
 
 const PostCard = ({post}) => {
     console.log(post)
     return (
         <article className="card mt-6 lg:mt-8">
             <PostHeader post={post} />
+            <PostBody poster={post?.image} content={post?.content}/>
+            <PostAction  postId={post?.id}
+                commentCount={post?.comments?.length}/>
+            <PostComments post={post}/>
+            <PostCommentList/>
 
         </article>
     );
--- src/components/posts/PostComments.jsx
+++ src/components/posts/PostComments.jsx
@@ -1,10 +1,39 @@
-import React from 'react';
+import { useState } from "react";
+import { useAvatar } from "../../hooks/useAvatar";
+import PostCommentList from "./PostCommentList";
 
-const PostComments = () => {
+const PostComments = ({ post }) => {
+    const [showAllComments,setShowAllComments] = useState(false);
+    const { avatarURL } = useAvatar(post);
+    const handleShowAllComments=()=>{
+        setShowAllComments(!showAllComments);
+    }
     return (
         <div>
-            PostComments
+            <div className="flex-center mb-3 gap-2 lg:gap-4">
+                <img
+                    className="max-w-7 max-h-7 rounded-full lg:max-h-[34px] lg:max-w-[34px]"
+                    src={avatarURL}
+                    alt="avatar"
+                />
 
+                <div className="flex-1">
+                    <input
+                        type="text"
+                        className="h-8 w-full rounded-full bg-lighterDark px-4 text-xs focus:outline-none sm:h-[38px]"
+                        name="post"
+                        id="post"
+                        placeholder="What's on your mind?"
+                    />
+                </div>
+            </div>
+            <div className="mt-4">
+                <button className="text-gray-300 max-md:text-sm" onClick={handleShowAllComments}>
+                    {showAllComments?"Hide All comments":"All Comment ▾"}
+                </button>
+            </div>
+
+         {   showAllComments?(<PostCommentList comments={post?.comments} />):('')}
         </div>
     );
 };
